d16c1654555461c69a108978b519d1f382014d60
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2022-02-03 22:55:59 +0530

    Basic game logic is in
    Two people who can identify who wins the game can play it now

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -4,7 +4,9 @@
 #include<conio.h>
 #include<io.h>
 using namespace std;
-
+string a1="a1",a2="a2",a3="a3",b1="b1",b2="b2",b3="b3",c1="c1",c2="c2",c3="c3";
+string ga1="\0",ga2="\0",ga3="\0",gb1="\0",gb2="\0",gb3="\0",gc1="\0",gc2="\0",gc3="\0"; //game board variable values
+string b = "\0";
 /*
 This is what the tic-tac-toe board is going to look like 
 		*		*
@@ -49,28 +51,53 @@ int clrscr()
 
 int StartGame()
 {
-	string a1="a1",a2="a2",a3="a3",b1="b1",b2="b2",b3="b3",c1="c1",c2="c2",c3="c3";
-	char x = 'X', o='O',b = '\0';    // char x = 2 or x = 1 will give a smiley unicode character , very cool. If possible replace the winning symbol with the smiley
+//	string a1="a1",a2="a2",a3="a3",b1="b1",b2="b2",b3="b3",c1="c1",c2="c2",c3="c3";
+//	 x = 'X', o='O',b = '\0';    // char x = 2 or x = 1 will give a smiley unicode character , very cool. If possible replace the winning symbol with the smiley
 	InitBoard(a1,a2,a3,
 			  b1,b2,b3,
 			  c1,c2,c3);
 	
-	cout<<"The game will be played using text input only.\nTo play you must enter the coordinate of the box first and then follow it up by an x or an o.\nYou can call up this coordinate board anytime while playing the game.(Game will be reset)\nPress Enter to clear this and start playing.";
+	cout<<"The game will be played using text input only.\nTo play you must enter the coordinate of the box first and then follow it up by an x or an o.\nYou can press capital R to call up this coordinate board anytime while playing the game.(Game will be reset)\nPress Enter to clear this and start playing.";
 	cin.get();
 	clrscr();
-	return 0;
-}
-
-
-int PlayGame()
-{
-	string x = "X", o="O",b="\0";
 	InitBoard(b,b,b,
 			  b,b,b,
 			  b,b,b);
-	cout<<"Enter the coordinate of a box :";
 	
+	return 0;
+}
+
+int UpdateBoard(string input , string choice)
+{
+	string R;
+if(input==a1)ga1=choice;
+else if(input==a2)ga2=choice;
+else if(input==a3)ga3=choice;
+else if(input==b1)gb1=choice;
+else if(input==b2)gb2=choice;
+else if(input==b3)gb3=choice;
+else if(input==c1)gc1=choice;
+else if(input==c2)gc2=choice;
+else if(input==c3)gc3=choice;
+else if(input==R){StartGame();}
+
+clrscr();
+InitBoard(ga1,ga2,ga3,gb1,gb2,gb3,gc1,gc2,gc3);
+	
+}
+
+int PlayGame()
+{
+	string x = "X", o="O",X="X",O="O",b="\0";
+	string input ,choice;
+	
+	cout<<"Enter the coordinate of a box :";cin>>input;cout<<endl;
+	cout<<"Enter X or O :";cin>>choice;cout<<endl;
+	cout<<"Enter R if you want to see the rules and coordinate board again :";
+	UpdateBoard(input,choice);
 	cin.get();
+	
+	PlayGame();
 	return 0;
 }
 
