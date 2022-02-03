788d7c7828aa3f95c18543b6aa7b0e5a53e5804c
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2022-02-03 19:54:54 +0530

    Added the StartGame function

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -21,7 +21,7 @@ This is what the tic-tac-toe board is going to look like
 
 */
 
-int DrawBoard(char a1,char a2,char a3,char b1,char b2,char b3,char c1,char c2,char c3)
+int InitBoard(string a1,string a2,string a3,string b1,string b2,string b3,string c1,string c2,string c3)
 {	
 //	char a1,a2,a3,b1,b2,b3,c1,c2,c3;
 	char x = 'X', o='O';
@@ -44,6 +44,28 @@ int DrawBoard(char a1,char a2,char a3,char b1,char b2,char b3,char c1,char c2,ch
 	
 }
 
+int UpdateBoard(char a1,char a2,char a3,char b1,char b2,char b3,char c1,char c2,char c3)
+{	
+//	char a1,a2,a3,b1,b2,b3,c1,c2,c3;
+	char x = 'X', o='O';
+	cout<<"\t\t*\t\t*\t\t"<<endl;
+	cout<<"\t"<<a1<<"\t*"<<"\t"<<a2<<"\t*"<<"\t"<<a3<<"\t"<<endl;
+	cout<<"\t\t*\t\t*\t\t"<<endl;
+	cout<<"**************************************************"<<endl;
+	cout<<"\t\t*\t\t*\t\t"<<endl;
+	cout<<"\t"<<b1<<"\t*"<<"\t"<<b2<<"\t*"<<"\t"<<b3<<"\t"<<endl;
+	cout<<"\t\t*\t\t*\t\t"<<endl;
+	cout<<"**************************************************"<<endl;
+	cout<<"\t\t*\t\t*\t\t"<<endl;
+	cout<<"\t"<<c1<<"\t*"<<"\t"<<c2<<"\t*"<<"\t"<<c3<<"\t"<<endl;
+	cout<<"\t\t*\t\t*\t\t"<<endl;
+	return 0;
+}
+	
+	
+	
+	
+
 int clrscr()
 {
 	system("cls");
@@ -51,13 +73,22 @@ int clrscr()
 
 int StartGame()
 {
-	char a1,a2,a3,b1,b2,b3,c1,c2,c3;
-	char x = 1, o='O',b = '\0';// char x = 2 or x = 1 will give a smiley unicode character , very cool. If possible replace the winning symbol with the smiley
-	DrawBoard(x,b,x,
-			  o,x,b,
-			  x,b,o);
+	string a1="a1",a2="a2",a3="a3",b1="b1",b2="b2",b3="b3",c1="c1",c2="c2",c3="c3";
+	char x = 'X', o='O',b = '\0';    // char x = 2 or x = 1 will give a smiley unicode character , very cool. If possible replace the winning symbol with the smiley
+	InitBoard(a1,a2,a3,
+			  b1,b2,b3,
+			  c1,c2,c3);
 	
+	cout<<"The game will be played using text input only.\nTo play you must enter the coordinate of the box first and then follow it up by an x or an o.\nYou can call up this coordinate board anytime while playing the game.(Game will be reset)\nPress Enter to clear this and start playing.";
+	cin.get();
+	clrscr();
 	return 0;
+}
+
+
+int PlayGame()
+{
+	
 }
 
 int main()
@@ -65,9 +96,5 @@ int main()
 	char a1,a2,a3,b1,b2,b3,c1,c2,c3;
 	char x = 'X', o='O',b = '\0'; 
 	StartGame();
-	
-	
-	
-	
 	
 }
